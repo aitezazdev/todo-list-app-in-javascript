5d3e12dfaa26f4383c1fad7af52633cbb4bdaaa1
Aitezaz Sikandar <aitezazsikandar@gmail.com>
2024-09-22 17:28:03 +0500

    btn styles change

--- app.js
+++ app.js
@@ -2,16 +2,16 @@ var todoBtn = document.getElementById("add-todo");
 var todoInput = document.getElementById("todo-input");
 var todoList = document.getElementById("todo-list");
 
-todoBtn.addEventListener("click", function () {
+function addTodo() {
   var todoText = todoInput.value;
   if (todoText) {
     var li = document.createElement("li");
     li.innerText = todoText;
     var btn = document.createElement("button");
+    btn.classList.add("remove-btn");
     btn.innerText = "Remove";
     li.appendChild(btn);
     todoList.appendChild(li);
   }
   todoInput.value = "";
-});
-
+}
